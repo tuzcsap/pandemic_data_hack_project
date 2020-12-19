99e6ed9ddc3e7216609e380d2355c35d9e5675e7
tuzcsap <mikhail.miazin@yahoo.com>
2020-12-19 14:58:38 +0300

    add large_fam observations

--- large_fam.R
+++ large_fam.R
@@ -1,25 +1,27 @@
+### многодетные
 library(tidyverse)
 
 unemp1 <- read_delim("/Users/mikhail/Desktop/Pandemic Data Hack/unemployed_1/unemployed_1_data.csv", delim=";")
 #sample <- read_delim("/Users/mikhail/Desktop/Pandemic Data Hack/unemployed_1/unemployed_1_sample.csv", delim=";")
 
-unemp1 %>% count(hc_singleparent)
+unemp1 %>% count(hc_largefam)
 
-# одинокие, не работали, их образование
+# многодетные, не работали, их образование
 unemp1 %>% 
   filter(hc_didnotwork) %>% 
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(education_filtered, sort = TRUE)
 
+# предпенсионеры
 unemp1 %>% 
   filter(hc_predpens) %>% 
-  count(hc_singleparent)
-  
+  count(hc_largefam)
+
 
 ## Регион
 #одинокие
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(region, sort = TRUE)
 # all
 unemp1 %>%
@@ -27,22 +29,28 @@ unemp1 %>%
 
 # Округа
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(district, sort = TRUE)
 # all
 unemp1 %>%
   count(district, sort = TRUE)
 
-
+## фильтр: высшее в одно значени
 unemp1 <- unemp1 %>% 
   #mutate(education_filtered = str_replace_all(education,"Высшее", "Высшее")) %>% 
   mutate(education_filtered = ifelse(str_detect(education, "Высшее"), "Высшее образование", education))
 
-# образование для одиночек
+# образование для многодетных
 unemp1 %>%
-  filter(hc_singleparent) %>% 
-  #filter(age==1) %>% 
+  filter(hc_largefam) %>% 
+  filter(age==1) %>% 
   count(education_filtered, sort = TRUE)
+
+# образование многодетных 16-17 лет
+unemp1 %>%
+  filter(hc_largefam) %>% 
+  filter(age==1) %>% 
+  View()
 
 # образование для всех
 unemp1 %>% 
@@ -50,42 +58,61 @@ unemp1 %>%
 
 # возрастные группы с начальным образованием
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   filter(education_filtered=="Начальное общее образование") %>% 
+  View()
   count(age, sort = TRUE)
 
-# образование по полу у одиночек
+# образование по полу у многодетных
 unemp1 %>%
   filter(gender=="Мужской") %>% 
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(education_filtered, sort = TRUE)
 
 unemp1 %>%
   filter(gender=="Женский") %>% 
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(education_filtered, sort = TRUE)
 
+# по полу
+unemp1 %>%
+  filter(hc_largefam) %>% 
+  count(gender)
+# доля
+unemp1 %>%
+  filter(hc_largefam) %>% 
+  group_by(gender) %>% 
+  count(gender) %>% 
+  ungroup() %>% 
+  mutate(ratio = n / sum(n))
 
 # расторжение
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(dismissal_reason, sort = TRUE)
 
 # профессия по последнему месту работы
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(profession_last_work, sort = TRUE)
 # и для всех
 unemp1 %>%
   count(profession_last_work, sort = TRUE)
 
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   filter(gender=="Мужской") %>% 
   count(profession_last_work, sort = TRUE)
 
+# котельные
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
+  filter(gender=="Женский") %>% 
+  filter(profession_last_work == "Машинист котельной установки") %>% 
+  count(region, sort = TRUE)
+
+unemp1 %>%
+  filter(hc_largefam) %>% 
   filter(gender=="Женский") %>% 
   count(profession_last_work, sort = TRUE)
 
@@ -101,35 +128,34 @@ unemp1 %>%
 
 # профессия по образованию
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(profession_last_educ, sort = TRUE)
 
 # пиздец
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   filter(profession_last_educ=="Программист") %>% 
   group_by(gender) %>% 
   count(gender) %>% 
   ungroup() %>% 
   mutate(ratio = n / sum(n))
 
-unemp1 %>%
-  filter(hc_singleparent) %>% 
-  filter(profession_last_educ=="Программист") %>% 
-  fi
+# unemp1 %>%
+#   filter(hc_largefam) %>% 
+#   filter(profession_last_educ=="Программист")
 
 # стаж в этом году
 unemp1 %>%
-  filter(hc_singleparent) %>% 
-  count(experience, sort = TRUE) %>% 
+  filter(hc_largefam) %>% 
+  count(experience, sort = TRUE)
   
-# нашли ли работу люди без опыта за 12 месяцев
-# all
+  # нашли ли работу люди без опыта за 12 месяцев
+  # all
 unemp1 %>%
   count(experience, sort = TRUE)
-# интересна доля одиночек с разным стажем
+# интересна доля многодетных с разным стажем
 unemp1 %>%
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   group_by(experience) %>% 
   count(experience) %>% 
   ungroup() %>% 
@@ -141,10 +167,10 @@ unemp1 %>%
   count(experience) %>% 
   ungroup() %>% 
   mutate(ratio = (n / sum(n)))
-  
-#  одиночек без опыта за год по регионам
+
+#  многодетные без опыта за год по регионам
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   filter(experience == "0.0") %>% 
   count(region, sort = TRUE)
 
@@ -155,7 +181,7 @@ unemp1 %>%
 
 # ЗП за 3 месяца!
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(salary_average, sort = TRUE)
 
 #all
@@ -163,89 +189,60 @@ unemp1 %>%
   count(salary_average, sort = TRUE)
 
 unemp1 %>%
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   filter(region == "г. Москва") %>% 
   count(salary_average, sort = TRUE)
 
 
 # ВОЗРАСТ
 unemp1 %>% 
-  filter(hc_singleparent) %>% 
+  filter(hc_largefam) %>% 
   count(age, sort = TRUE)
 
 # среди всех
 unemp1 %>% 
   count(age, sort = TRUE)
 
-
-### ПОЛ
-unemp1 %>% 
-  filter(hc_singleparent) %>% 
-  count(gender, sort = TRUE)
-
-# доля
-unemp1 %>%
-  filter(hc_singleparent) %>% 
-  group_by(gender) %>% 
-  count(gender) %>% 
-  ungroup() %>% 
-  mutate(ratio = n / sum(n))
-
-# общее
-unemp1 %>%
-  group_by(gender) %>% 
-  count(gender) %>% 
-  ungroup() %>% 
-  mutate(ratio = n / sum(n))
-
-
-# среди всех
-unemp1 %>% 
-  count(gender, sort = TRUE)
 
 # МЕСЯЦ УВОЛЬНЕНИЯ 
 unemp1 %>% 
-  filter(hc_singleparent) %>% 
-  count(month_dismissal)
+  filter(hc_largefam) %>% 
+  count(month_dismissal, sort = TRUE)
 ###
 # когда увольняли(сь)
 unemp1 %>% 
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   count(month_dismissal) %>% 
   ggplot(aes(month_dismissal, n))+
   geom_col()
 # когда обратились в СЗН
 unemp1 %>% 
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   count(month_unemployed) %>% 
   ggplot(aes(month_unemployed, n))+
   geom_col()
 
 # устроились
 unemp1 %>% 
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   count(month_employment) %>% 
   ggplot(aes(month_employment, n))+
   geom_col()
 
 # снятие с учета как безработного
 unemp1 %>% 
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   count(month_close) %>% 
   ggplot(aes(month_close, n))+
   geom_col()
 
-# тип трудоустройства одиночек и всех
+# тип трудоустройства многодетных и всех
 unemp1 %>% 
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   count(employment_type, sort = TRUE)
 
 unemp1 %>% 
-  filter(hc_singleparent) %>%
+  filter(hc_largefam) %>%
   count(employment_mode, sort = TRUE)
-
-#######################################
-## Многодетные
-#######################################
 
 

33d5de9185185934fc60fd5e1fde10de828ce0fb
tuzcsap <mikhail.miazin@yahoo.com>
2020-12-19 12:51:24 +0300

    more observations

--- main.R
+++ main.R
@@ -13,10 +13,42 @@ unemp1 %>%
   filter(hc_singleparent) %>% 
   count(region, sort = TRUE)
 
-# образование -> объединить высшие
+unemp1 <- unemp1 %>% 
+  #mutate(education_filtered = str_replace_all(education,"Высшее", "Высшее")) %>% 
+  mutate(education_filtered = ifelse(str_detect(education, "Высшее"), "Высшее образование", education))
+
+# образование для одиночек
 unemp1 %>%
   filter(hc_singleparent) %>% 
-  count(education, sort = TRUE)
+  #filter(age==1) %>% 
+  count(education_filtered, sort = TRUE)
+
+# образование для всех
+unemp1 %>% 
+  count(education_filtered, sort = TRUE)
+
+# возрастные группы с начальным образованием
+unemp1 %>%
+  filter(hc_singleparent) %>% 
+  filter(education_filtered=="Начальное общее образование") %>% 
+  count(age, sort = TRUE)
+
+# образование по полу у одиночек
+unemp1 %>%
+  filter(gender=="Мужской") %>% 
+  filter(hc_singleparent) %>% 
+  count(education_filtered, sort = TRUE)
+
+unemp1 %>%
+  filter(gender=="Женский") %>% 
+  filter(hc_singleparent) %>% 
+  count(education_filtered, sort = TRUE)
+
+
+# расторжение
+unemp1 %>%
+  filter(hc_singleparent) %>% 
+  count(dismissal_reason, sort = TRUE)
 
 # профессия по последнему месту работы
 unemp1 %>%
@@ -36,7 +68,7 @@ unemp1 %>%
 unemp1 %>%
   filter(hc_singleparent) %>% 
   count(district, sort = TRUE)
-
+# ВОЗРАСТ
 unemp1 %>% 
   filter(hc_singleparent) %>% 
   count(age, sort = TRUE)
@@ -45,6 +77,8 @@ unemp1 %>%
 unemp1 %>% 
   count(age, sort = TRUE)
 
+
+### ПОЛ
 unemp1 %>% 
   filter(hc_singleparent) %>% 
   count(gender, sort = TRUE)
@@ -52,14 +86,9 @@ unemp1 %>%
 # среди всех
 unemp1 %>% 
   count(gender, sort = TRUE)
+
 
 ###
 
-unemp1 %>%
-  filter(hc_largefam) %>% 
-  count(region, sort = TRUE)
 
-unemp1 %>%
-  filter(hc_largefam) %>% 
-  count(gender, sort = TRUE)
 
